2bd8f736bde7da763ccd6fe3f46cb2d6ff55b196
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2022-05-13 18:51:37 -0300

    database

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-52-200-215-149.compute-1.amazonaws.com'),
+      host: env('DATABASE_HOST'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd1hrk1kduf4jnf'),
-      user: env('DATABASE_USERNAME', 'rculhqrqqigrrq'),
-      password: env('DATABASE_PASSWORD', '2fae638851e5259d1af6ae8d403dc1f44f9aeb8430ce6ce553a975073737c906'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
       },
